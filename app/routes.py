bf9a366adab0fb2637edfe3fade1970955811a65
nsandarma <nsandarma@gmail.com>
2023-10-13 02:06:06 +0700

    del get_model

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template,request
 from werkzeug.utils import secure_filename
 from . import app
-from .utils import predict,get_model
+from .utils import predict
 import os
 
 @app.route('/',methods=['GET','POST'])
